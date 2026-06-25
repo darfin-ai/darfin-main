57cfd4df857181a2cc0a00ecb41191eb4123bed4
sanghyxuk <pyosang0919@gmail.com>
2026-06-25 14:37:37 +0900

    feat: Spring boot MVC 구조 초기 세팅

---
+++ src/main/java/com/kosta/darfin/DarfinMainApplication.java
@@ -0,0 +1,13 @@
+package com.kosta.darfin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarfinMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DarfinMainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kosta/darfin/DarfinMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kosta.darfin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DarfinMainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
